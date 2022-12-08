ea8a3ee317c13982287e6212f6bf897085753aa8
Paul-Antoine Fruchtenreich <paul-antoine.fruchtenreich@ens.psl.eu>
2022-12-08 18:33:02 +0100

    données rééquilibrées

--- projet-python.py
+++ projet-python.py
@@ -197,8 +197,16 @@ Y6 = test6['grav']
 X_train6 = normalize(X6.values)
 
 #REBALANCE
-sm = SMOTE(k_neighbors=2, sampling_strategy=0.3)
+sm = SMOTE()
 Xsmoted, Ysmoted = sm.fit_resample(X_train6, Y8)
+np.unique(Ysmoted, return_counts = True)
+
+labelssm, countsm = np.unique(Ysmoted, return_counts=True)
+plt.bar(labelssm, countsm/len(Ysmoted)*100, align='center')
+plt.gca().set_xticks(labelssm)
+plt.show()
+
+#BEFORE REBALANCE
 
 #RANDOM FOREST
 xtrain, xtest, ytrain, ytest = train_test_split(X_train6,Y8)
@@ -223,86 +231,22 @@ modelxgb.fit(X6_train,Y6_train)
 Y6_pred2 = modelxgb.predict(X6_test)
 test_acc2 = accuracy_score(Y6_test, Y6_pred2)
 
+#AFTER REBALANCE
 
+#RANDOM FOREST
+xtrain_reb, xtest_reb, ytrain_reb, ytest_reb = train_test_split(Xsmoted,Ysmoted)
+model_rf_prime = RandomForestClassifier(n_estimators=50, 
+                                  max_depth=5)
+model_rf_prime.fit(xtrain_reb, ytrain_reb)
+ypredrf_reb = model_rf_prime.predict(xtest_reb)
+test_acc_reb = accuracy_score(ytest_reb, ypredrf_reb)
 
+cmrf_reb = confusion_matrix(ytest_reb, ypredrf_reb,labels=model_rf_prime.classes_)
+disprf_reb = ConfusionMatrixDisplay(cmrf_reb,display_labels=model_rf_prime.classes_)
+disprf_reb.plot()
+plt.show()
 
-#valeurs = lieux['Num_Acc']
-#valeurs2 = valeurs.to_numpy()
-#repet = usagers['Num_Acc']
-#repet2 = repet.to_numpy()
+recall = recall_score(ytest, ypredrf, average='weighted')
+f1 = f1_score(ytest, ypredrf, average='weighted')
 
-#valeurs3 = vehicules['id_vehicule']
-#valeurs4 = valeurs3.to_numpy()
-#repet3 = usagers['id_vehicule']
-#repet4 = repet3.to_numpy()
-
-#but créer un df contenant les infos de test mais avec des lignes doublées en fn du nb de passagers dans la voiture
-
-#def compte(valeur,repet):
-    #compte le nombre de répétitions de lignes pour un accident dans test2 
-    #L = []
-    #nn = len(valeur)
-    #for i in range(nn):
-        #a = valeur[i]
-        #c = np.count_nonzero(repet == a)
-        #L.append(c)
-   #return L
-        
-
-#liste = compte(valeurs2,repet2)
-# comment passer de 56518 à 129153
-
-#liste2 =compte(valeurs4,repet4)
-
-#fruit_list = [ ('Orange', 34, 'Yes' )]
-#df = pd.DataFrame(fruit_list, columns = ['Name' , 'Price', 'Stock'])
-
-#def creat(vehicules,liste):
-    #vehicules2 = vehicules.copy()
-    #l = len(liste)
-    #ll = 0
-    #for i in range(l):
-        #if liste[i] !=1:
-            #for j in range(liste[i]-1):
-                #vehicules2.loc[l+ll] = vehicules.iloc[i]
-                #ll+=1
-    #return vehicules2
-            
-#on crée les lignes doublons dans véhicules qui correspondent aux usagers d'un même véhicule afin de concat avec usagers ensuite par les colonnes
-#vehicules2 = creat(vehicules,liste2)
-
-#vehicules3 = vehicules2.sort_values(by = ['Num_Acc', 'id_vehicule'])    
-    
-#6 lignes de décalage dans les deux dataframes véhicules3 et usagers, jsp pq
-#vehicules4 = vehicules3.reset_index(drop=True)
-    
-
-#bug = vehicules5['Num_Acc']
-#bug2 = bug.to_numpy()
-#bugprime = usagers['Num_Acc']
-#bugprime2 = bugprime.to_numpy()   
-   
-
-#A = compte(valeurs2,bug2)
-#B = compte(valeurs2,bugprime2)
-
-#def diff(A,B):
-#    l = len(A)
-    #L = []
-    #for i in range(l):
-        #L.append(A[i]-B[i])
-    #return L
-
-#lignes de vehicules4 à drop : 
-    #première de Num_Acc = 202100009430
-    #dernière de Num_Acc = 202100022987
-    #dernière de Num_Acc = 202100040202
-    #première de Num_Acc = 202100040933
-    #première de Num_Acc = 202100044328
-    #première de Num_Acc = 202100053167
-    
-    
-#vehicules5 = vehicules4.drop(index = [21787,52699,92651,94291,101765,121556])
-
-#test2 = pd.concat([vehicules5,usagers], axis = 1)
 
